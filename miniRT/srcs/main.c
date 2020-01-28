c5ff96a1fe462b1962ee5c9cb1e4d29296a7c430
Celestin MEUNIER <cmeunier@e2r3p10.42.fr>
2020-01-28 19:09:31 +0100

    display sphere

--- miniRT/srcs/main.c
+++ miniRT/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cmeunier <cmeunier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 17:25:41 by cmeunier          #+#    #+#             */
-/*   Updated: 2020/01/27 21:37:33 by cmeunier         ###   ########.fr       */
+/*   Updated: 2020/01/28 19:01:03 by cmeunier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ double get_vp_x(int x)
 {
 	double vp_x;
 
-	vp_x = x * VIEWPORT_WIDTH / WINDOW_WIDTH;
+	vp_x = (double)x * VIEWPORT_WIDTH / WINDOW_WIDTH;
 	return(vp_x);
 }
 
@@ -56,7 +56,7 @@ double get_vp_y(int y)
 {
 	double vp_y;
 
-	vp_y = y * VIEWPORT_HEIGHT / WINDOW_HEIGHT;
+	vp_y = (double)y * VIEWPORT_HEIGHT / WINDOW_HEIGHT;
 	return(vp_y);
 }
 
@@ -69,7 +69,7 @@ void intersect_ray_sphere(t_ray *ray, t_camera *camera, t_point *viewport_point,
     double k2 = 2 * prod_scal(oc, *viewport_point);
     double k3 = prod_scal(oc, oc) - (sphere->r * sphere->r);
 
-    double discriminant = k2 * k2 - 4 * k1 * k3;
+    double discriminant = (k2 * k2) - (4 * k1 * k3);
     if(discriminant < 0)
 	{
 		ray->t1 = __DBL_MAX__;
@@ -92,9 +92,9 @@ int		trace_ray(t_camera *camera, t_point *viewport_point, double min_z, double m
 	// this should be included in a loop to enable every sphere
 	sphere_0.pos.x = 0;
 	sphere_0.pos.y = 0;
-	sphere_0.pos.z = 30;
-	sphere_0.r = 5;
-	sphere_0.color = get_color_integer(0, 3, 104);;
+	sphere_0.pos.z = 10;
+	sphere_0.r = 2;
+	sphere_0.color = get_color_integer(0, 3, 104);
 	closest_t = max_z;
 	closest_sphere = NULL;
 	intersect_ray_sphere(&ray, camera, viewport_point, &sphere_0);
@@ -110,8 +110,9 @@ int		trace_ray(t_camera *camera, t_point *viewport_point, double min_z, double m
 	}
 	// put white if no sphere interesection was found.
 	if(closest_sphere == NULL)
-		color = BACKGROUND_COLOR;
-	color = closest_sphere->color;
+		color = get_color_integer(255, 255, 255);
+	else
+		color = closest_sphere->color;
 	return(color);
 }
 
@@ -144,8 +145,12 @@ int		main(int ac, char **av)
 				viewport_point.y = get_vp_y(y);
 				viewport_point.z = VIEWPORT_D;
 				color = trace_ray(&camera, &viewport_point, VIEWPORT_D, __DBL_MAX__);
+				//color = get_color_integer(255, 255, 255);
 				mlx_pixel_put(mlx_ptr, win_ptr, center_x(x), center_y(y), color);
 				y++;
+				printf("viewport.x: %f\n", viewport_point.x);
+				printf("viewport.y: %f\n", viewport_point.y);
+				printf("viewport.z: %f\n", viewport_point.z);
 			}
 			y = - WINDOW_HEIGHT / 2;
 			x++;
