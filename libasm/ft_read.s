e7e0f16045c2a10a8804ae008b7a33f9e8b35649
cmeunier <cmeunier@student.42.fr>
2020-05-19 16:55:19 +0200

    added errno location

--- libasm/ft_read.s
+++
@@ -1,7 +0,0 @@
-			section	.text
-			global	_ft_read
-
-_ft_read:
-			mov		rax, 0x2000003 ;syscall for read is 3 on MacOs and 0 on linux
-			syscall
-			ret
--- libasm/libasm-lnx/ft_write.s
+++
@@ -1,7 +0,0 @@
-            section	.text
-			global	ft_write
-
-ft_write:
-            mov         rax, 0x2000004 ;syscall for write is 4 on MacOs and 1 on linux
-            syscall
-            ret
